cba0267500037681974fe525fefc44221e33ad35
ThetNaingTun <kothetnaingtun.ict@gmail.com>
2025-04-07 21:52:43 +0630

    start hr

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView, View, CreateView, DetailView,FormView
+from django.urls import reverse_lazy
+from django.db.models import Avg, Max, Min, Sum
+# Create your views here.
+# Import necessary modules and models
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
+from django.contrib.auth import authenticate, login, logout
+from django.core.paginator import Paginator
 
 # Create your views here.
